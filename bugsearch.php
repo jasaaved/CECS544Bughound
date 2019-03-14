3d613499fbd7585c496eb18b01ce0d2790136bea
Christopher Gutierrez <Christopher.Gutierrez02@student.csulb.edu>
2019-03-13 22:40:10 -0700

    front end to bugsearch mostly done.

--- bugsearch.php
+++ bugsearch.php
@@ -61,30 +61,172 @@
                     ?>
                 </select>
                 
-                                <label for="severity">Severity:</label>
-				<select name="severity">
-					<option value=null></option>
-					<?php
-						$query = "SELECT TRIM(TRAILING ')' FROM TRIM(LEADING '(' FROM TRIM(LEADING 'enum' FROM column_type))) column_type FROM information_schema.columns WHERE table_name = 'bug_report' AND column_name = 'severity'";
-						
-						if ($result = mysqli_query($con, $query))
-						{
-							$row=mysqli_fetch_row($result);
-							$enum=explode(',',$row[0]);
-							
-							for ($i=0; $i < sizeOf($enum); $i++)
-							{
-								$str = str_replace('\'', '', $enum[$i]);
-								$val = $i+1;
-								echo "<option value=$val>$str</option>";
-							}
-						}
-					?>
-				
-				</select>
+                <label for="severity">Severity:</label>
+                <select name="severity">
+                        <option value=null></option>
+                        <?php
+                                $query = "SELECT TRIM(TRAILING ')' FROM TRIM(LEADING '(' FROM TRIM(LEADING 'enum' FROM column_type))) column_type FROM information_schema.columns WHERE table_name = 'bug_report' AND column_name = 'severity'";
+
+                                if ($result = mysqli_query($con, $query))
+                                {
+                                        $row=mysqli_fetch_row($result);
+                                        $enum=explode(',',$row[0]);
+
+                                        for ($i=0; $i < sizeOf($enum); $i++)
+                                        {
+                                                $str = str_replace('\'', '', $enum[$i]);
+                                                $val = $i+1;
+                                                echo "<option value=$val>$str</option>";
+                                        }
+                                }
+                        ?>
+
+                </select>
+                
+                <br>
+                <br>
+                                
+                        <label for="functional_area">Functional Area:</label>
+			<select name="functional_area">
+                            <option value=null></option>
+                            <?php
+                                    $query = "SELECT * FROM functional_area";
+                                    $result = mysqli_query($con, $query);
+
+                                    while($row=mysqli_fetch_row($result))
+                                    {
+                                            echo "<option value=$row[0]>$row[1]</option>";
+                                    }
+                            ?>
+			</select>
+                        
+                        <label for="assigned_to">Assigned To:</label>
+                        <select name="assigned_to">
+                            <option value=null></option>
+                            <?php
+                                $query = "SELECT * FROM employee";
+                                $result = mysqli_query($con, $query);
+
+                                while($row=mysqli_fetch_row($result))
+                                {
+                                    echo "<option value=$row[0]>$row[1]</option>";
+                                }
+                            ?>
+			</select>
+                        
+                        <br>
+                        <br>
+                        
+                        <label for="status">Status:</label>
+			<select name="status">
+                            <option value=null></option>
+                            <?php
+                                $query = "SELECT TRIM(TRAILING ')' FROM TRIM(LEADING '(' FROM TRIM(LEADING 'enum' FROM column_type))) column_type FROM information_schema.columns WHERE table_name = 'bug_report' AND column_name = 'status'";
+
+                                if ($result = mysqli_query($con, $query))
+                                {
+                                    $row=mysqli_fetch_row($result);
+                                    $enum=explode(',',$row[0]);
+
+                                    for ($i=0; $i < sizeOf($enum); $i++)
+                                    {
+                                            $str = str_replace('\'', '', $enum[$i]);
+                                            $val = $i+1;
+                                            echo "<option value=$val>$str</option>";
+                                    }
+                                }
+                            ?>
+			</select>
+                        
+                        <label for="priority">Priority:</label>
+                        <select name="priority">
+                            <option value=null></option>
+                            <?php
+                                $query = "SELECT TRIM(TRAILING ')' FROM TRIM(LEADING '(' FROM TRIM(LEADING 'enum' FROM column_type))) column_type FROM information_schema.columns WHERE table_name = 'bug_report' AND column_name = 'priority'";
+
+                                if ($result = mysqli_query($con, $query))
+                                {
+                                        $row=mysqli_fetch_row($result);
+                                        $enum=explode(',',$row[0]);
+
+                                        for ($i=0; $i < sizeOf($enum); $i++)
+                                        {
+                                                $str = str_replace('\'', '', $enum[$i]);
+                                                $val = $i+1;
+                                                echo "<option value=$val>$str</option>";
+                                        }
+                                }
+                            ?>
+			</select>
+                        
+                        <label for="resolution">Resolution:</label>
+			<select name="resolution">
+                            <option value=null></option>
+                            <?php
+                                $query = "SELECT TRIM(TRAILING ')' FROM TRIM(LEADING '(' FROM TRIM(LEADING 'enum' FROM column_type))) column_type FROM information_schema.columns WHERE table_name = 'bug_report' AND column_name = 'resolution'";
+
+                                if ($result = mysqli_query($con, $query))
+                                {
+                                    $row=mysqli_fetch_row($result);
+                                    $enum=explode(',',$row[0]);
+
+                                    for ($i=0; $i < sizeOf($enum); $i++)
+                                    {
+                                            $str = str_replace('\'', '', $enum[$i]);
+                                            $val = $i+1;
+                                            echo "<option value=$val>$str</option>";
+                                    }
+                                }
+                            ?>
+			</select>
+                        
+                        <br>
+                        <br>
+                        
+                        <label for="reported_by">Reported By:</label>
+                        <select name="reported_by">
+                            <option value=null></option>
+                            <?php
+                                $query = "SELECT * FROM employee";
+                                $result = mysqli_query($con, $query);
+
+                                while($row=mysqli_fetch_row($result))
+                                {
+                                        echo "<option value=$row[0]>$row[1]</option>";
+                                }
+                            ?>
+                        </select>
+                        
+                        <label for="date">Report Date:</label><input type="date" name="date">
+                        
+                        <label for="resolved_by">Resolved By:</label>
+			<select name="resolved_by">
+                            <option value=null></option>
+                            <?php
+                                $query = "SELECT * FROM employee";
+                                $result = mysqli_query($con, $query);
+
+                                while($row=mysqli_fetch_row($result))
+                                {
+                                    echo "<option value=$row[0]>$row[1]</option>";
+                                }
+                            ?>
+			</select>
+                        
+                        <br>
+                        <br>
+                        
+                        <input type="submit" name="Submit" value="Submit"/>
+			<input type="reset" value="Reset"/>
+			<input type="button" value="Cancel" id=button2 name=button2 onclick="go_home()"/>
         </form>
         
-        
+        <script language=Javascript>
+            function go_home(){
+                window.location.replace("index.php?user_name=<?php echo $username?>");
+            }
+            
+        </script>
         
         
     </body>
