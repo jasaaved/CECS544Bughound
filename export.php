94b601ff965cd156d2f8d85a5ab66f5947fa34bd
jasaaved <jasaaved@uci.edu>
2019-03-14 15:00:02 -0700

    Added export ascii

---
+++ export.php
@@ -0,0 +1,84 @@
+<!DOCTYPE html>
+<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.4.4/jquery.min.js"></script>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Export</title>
+    </head>
+    <body>
+
+        <h1>Export Database Data</h1>
+			
+         <form action="index.php?user_name=<?php echo $var; ?>" method="post" onsubmit="return validate(this)">
+            <table>
+            <tr><TD class = "select",>Database Tables:         
+    </TD>   
+    <TD>
+       <select name = "Database">        
+            <option value="1">All</option>
+            <option value="2">Bug Reports</option>
+            <option value="3">Programs</option>
+            <option value="4">Functional Areas</option>
+			<option value="5">Functional Areas in Programs</option>
+			<option value="6">Employees</option>
+			<option value="7">Attachments</option>
+       </select>
+    </TD>        
+</TR>
+           <tr><TD class = "select" id="format">Format:         
+    </TD>   
+    <TD>
+       <select name = "Format">        
+            <option value="1">ASCII</option>
+            <option value="2">XML</option>
+ 
+       </select>
+    </TD>        
+</TR>					
+            </table>
+            <input type="submit" name="Submit" value="Submit">
+			<input type="button" value="Cancel" id=button1 name=button1 onclick="go_home()">
+        </form>
+	<p>
+	<h3>
+	</h3>
+	</p>
+        <script language=Javascript>
+            function validate(theform) {
+				ajax =  do_ajax(theform.Database.value, theform.Format.value);
+				return false;
+            }
+			function getQueryVariable(variable)
+			{
+				   var query = window.location.search.substring(1);
+				   var vars = query.split("&");
+				   for (var i=0;i<vars.length;i++) {
+						   var pair = vars[i].split("=");
+						   if(pair[0] == variable){return pair[1];}
+				   }
+				   return(false);
+			}
+			
+			function do_ajax(database, format){
+				return theAjax("Export", database, format);
+			}
+			
+			function theAjax(method, database, format){
+				return $.ajax({
+					url: 'verifylogin.php',
+					type: 'POST',
+					data: {method: method, database:database, format:format},
+					success: function(data){
+						alert(data);
+
+					}
+
+				});
+			}
+			function go_home(){
+				var usrname = getQueryVariable("user_name");
+				window.location.replace("index.php?user_name="+usrname);
+            }
+</script>
+    </body>
+</html>
--- verifylogin.php
+++ verifylogin.php
@@ -206,7 +206,247 @@
 			mysqli_query($con, $query);
 			$query = "DELETE FROM functional_area WHERE id = '".$ID."'";
 			mysqli_query($con, $query);
-		 }		 
+		 }
+
+		function export(){
+			$con = mysqli_connect("localhost","root");
+		    mysqli_select_db($con, "bughound");
+			$database_number = $_POST['database'];
+			$format = $_POST['format'];
+			if ($database_number == 2 && $format == 1){
+					$query = "SELECT * FROM bug_report";
+					$result=mysqli_query($con, $query);
+					$file = "bugreport.txt";
+					$fh = fopen($file, 'a') or die("can't open file");
+					
+				while($row=mysqli_fetch_array($result)){
+					$ID = $row[0];
+					$programId = $row[1];
+					$report_type = $row[2];
+					$severity = $row[3];
+					$problem_summary = $row[4];
+					$reproducible = $row[5];
+					$problem = $row[6];
+					$suggested_fix = $row[7];
+					$reported_by = $row[8];
+					$reported_date = $row[9];
+					$attachmentid = $row[10];
+					$funct = $row[11];
+					$assignto = $row[12];
+					$comments = $row[13];
+					$status = $row[14];
+					$prio = $row[15];
+					$res = $row[16];
+					$res_version = $row[17];
+					$resolved_by = $row[18];
+					$resolveddate = $row[19];
+					$tested_by = $row[20];
+					$testeddate = $row[21];
+					$deferred = $row[22];
+					$bugs = "Bug Report ID: $ID, Program ID: $programId,  Report Type: $report_type, Severity: $severity, Problem Summary: $problem_summary, Reproducible: $reproducible, Problem: $problem, Suggested Fix: $suggested_fix, Reported By: $reported_by, Reported Date: $reported_date, AttachmentID: $attachmentid, Function Area ID: $funct, Assigned to: $assignto, Comments: $comments, Status: $status, Priority: $prio, Resolution: $res, Resolved Version: $res_version, Resolved By: $resolved_by, Resolved Date: $resolveddate, Tested By: $tested_by,Tested Date: $testeddate, Treat as Deferred: $deferred\n";
+					fwrite($fh, $bugs);
+				}
+				fwrite($fh, "\n");
+				fclose($fh);
+				
+			}
+			else if ($database_number == 3 && $format == 1){
+					$query = "SELECT * FROM program";
+					$result=mysqli_query($con, $query);
+					$file = "programs.txt";
+					$fh = fopen($file, 'a') or die("can't open file");
+					
+				while($row=mysqli_fetch_array($result)){
+					$ID = $row[0];
+					$name = $row[1];
+					$version_number = $row[2];
+					$release_number = $row[3];
+					$functions = "Program ID: $ID, Program Name: $name, Version Number: $version_number, Release Version: $release_number\n";
+					fwrite($fh, $functions);
+				}
+				fwrite($fh, "\n");
+				fclose($fh);
+				
+			}
+			else if ($database_number == 4 && $format == 1){
+					$query = "SELECT * FROM functional_area";
+					$result=mysqli_query($con, $query);
+					$file = "functionalareas.txt";
+					$fh = fopen($file, 'a') or die("can't open file");
+					
+				while($row=mysqli_fetch_array($result)){
+					$ID = $row[0];
+					$name = $row[1];
+					$functions = "Function Area ID: $ID, Function Area Name: $name\n";
+					fwrite($fh, $functions);
+				}
+				fwrite($fh, "\n");
+				fclose($fh);
+				
+			}
+			else if ($database_number == 5 && $format == 1){
+					$query = "SELECT * FROM functional_and_prog";
+					$result=mysqli_query($con, $query);
+					$file = "areasinprograms.txt";
+					$fh = fopen($file, 'a') or die("can't open file");
+					
+				while($row=mysqli_fetch_array($result)){
+					$ID = $row[0];
+					$programid = $row[1];
+					$functionname = $row[2];
+					$functionid = $row[3];
+					$functions = "ID: $ID Program ID: $programid, Function Area Name: $functionname, Function ID: $functionid\n";
+					fwrite($fh, $functions);
+				}
+				fwrite($fh, "\n");
+				fclose($fh);
+				
+			}
+			else if ($database_number == 6 && $format == 1){
+					$query = "SELECT * FROM employee";
+					$result=mysqli_query($con, $query);
+					$file = "employees.txt";
+					$fh = fopen($file, 'a') or die("can't open file");
+					
+				while($row=mysqli_fetch_array($result)){
+					$ID = $row[0];
+					$name = $row[1];
+					$user_name = $row[2];
+					$password = $row[3];
+					$user_level = $row[4];
+					$functions = "Employee ID: $ID Employee Name: $name, User Name: $user_name, Password: $password, User level: $user_level\n";
+					fwrite($fh, $functions);
+				}
+				fwrite($fh, "\n");
+				fclose($fh);
+				
+			}
+			else if ($database_number == 7 && $format == 1){
+					$query = "SELECT * FROM attachment";
+					$result=mysqli_query($con, $query);
+					$file = "attachments.txt";
+					$fh = fopen($file, 'a') or die("can't open file");
+					
+				while($row=mysqli_fetch_array($result)){
+					$ID = $row[0];
+					$name = $row[1];
+					$functions = "Attachment ID: $ID Attachment Name: $name\n";
+					fwrite($fh, $functions);
+				}
+				fwrite($fh, "\n");
+				fclose($fh);
+				
+			}
+			
+			else if ($database_number == 1 && $format == 1){
+					$file = "all.txt";
+					$fh = fopen($file, 'a') or die("can't open file");
+
+					
+					
+				$query = "SELECT * FROM bug_report";
+					$result=mysqli_query($con, $query);
+					
+				while($row=mysqli_fetch_array($result)){
+					$ID = $row[0];
+					$programId = $row[1];
+					$report_type = $row[2];
+					$severity = $row[3];
+					$problem_summary = $row[4];
+					$reproducible = $row[5];
+					$problem = $row[6];
+					$suggested_fix = $row[7];
+					$reported_by = $row[8];
+					$reported_date = $row[9];
+					$attachmentid = $row[10];
+					$funct = $row[11];
+					$assignto = $row[12];
+					$comments = $row[13];
+					$status = $row[14];
+					$prio = $row[15];
+					$res = $row[16];
+					$res_version = $row[17];
+					$resolved_by = $row[18];
+					$resolveddate = $row[19];
+					$tested_by = $row[20];
+					$testeddate = $row[21];
+					$deferred = $row[22];
+					$bugs = "Bug Report ID: $ID, Program ID: $programId,  Report Type: $report_type, Severity: $severity, Problem Summary: $problem_summary, Reproducible: $reproducible, Problem: $problem, Suggested Fix: $suggested_fix, Reported By: $reported_by, Reported Date: $reported_date, AttachmentID: $attachmentid, Function Area ID: $funct, Assigned to: $assignto, Comments: $comments, Status: $status, Priority: $prio, Resolution: $res, Resolved Version: $res_version, Resolved By: $resolved_by, Resolved Date: $resolveddate, Tested By: $tested_by,Tested Date: $testeddate, Treat as Deferred: $deferred\n";
+					fwrite($fh, $bugs);
+				}
+				fwrite($fh, "\n");
+				$result=mysqli_query($con, $query);
+					
+					
+				while($row=mysqli_fetch_array($result)){
+					$ID = $row[0];
+					$name = $row[1];
+					$version_number = $row[2];
+					$release_number = $row[3];
+					$functions = "Program ID: $ID, Program Name: $name, Version Number: $version_number, Release Version: $release_number\n";
+					fwrite($fh, $functions);
+				}
+				fwrite($fh, "\n");
+					$query = "SELECT * FROM functional_area";
+					$result=mysqli_query($con, $query);
+				
+					
+				while($row=mysqli_fetch_array($result)){
+					$ID = $row[0];
+					$name = $row[1];
+					$functions = "Function Area ID: $ID, Function Area Name: $name\n";
+					fwrite($fh, $functions);
+				}
+				fwrite($fh, "\n");
+				
+					$query = "SELECT * FROM functional_and_prog";
+					$result=mysqli_query($con, $query);
+
+					
+				while($row=mysqli_fetch_array($result)){
+					$ID = $row[0];
+					$programid = $row[1];
+					$functionname = $row[2];
+					$functionid = $row[3];
+					$functions = "ID: $ID Program ID: $programid, Function Area Name: $functionname, Function ID: $functionid\n";
+					fwrite($fh, $functions);
+				}
+				fwrite($fh, "\n");
+				
+					$query = "SELECT * FROM employee";
+					$result=mysqli_query($con, $query);
+				
+					
+				while($row=mysqli_fetch_array($result)){
+					$ID = $row[0];
+					$name = $row[1];
+					$user_name = $row[2];
+					$password = $row[3];
+					$user_level = $row[4];
+					$functions = "Employee ID: $ID Employee Name: $name, User Name: $user_name, Password: $password, User level: $user_level\n";
+					fwrite($fh, $functions);
+				}
+				fwrite($fh, "\n");
+				
+				$query = "SELECT * FROM attachment";
+					$result=mysqli_query($con, $query);
+
+					
+				while($row=mysqli_fetch_array($result)){
+					$ID = $row[0];
+					$name = $row[1];
+					$functions = "Attachment ID: $ID Attachment Name: $name\n";
+					fwrite($fh, $functions);
+				}
+				fwrite($fh, "\n");
+				
+
+				fclose($fh);
+				
+			}
+			
+			
+		}		
 		
          echo $_POST["method"]();
 ?>
